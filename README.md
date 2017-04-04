bcedfb76987802a030315d161b4040c0e09c9c4a
Angus Lam <angus2651@gmail.com>
2017-04-05 00:47:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Awesome-Category
+
+# How to Use
+Step 1:
+Copy files to your Project
+
+Step 2:
+#import "UIColor+Hexadecimal.h"
+
+Step 3:
+For Example: 
+HexString is #F6F6F6
+add [UIColor colorWithHexString:@"#F6F6F6"] in the code.
---
+++ UIColor+Hexadecimal.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Hexadecimal.h
+//  EarnMoney
+//
+//  Created by Angus on 30/3/2017.
+//  Copyright © 2017 Angus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Hexadecimal)
+
++ (UIColor *)colorWithHexString:(NSString *)hexString;
+
+@end
